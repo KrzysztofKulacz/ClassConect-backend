095aa351255526d842b6b76c703bb5673365dd09
Krzysztof <krzysztof.mckulacz@gmail.com>
2022-10-21 12:09:33 +0200

    Initial commit. Let's go !!!

---
+++ src/main/java/com/example/classconectbackend/ClassConectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.classconectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassConectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassConectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classconectbackend/ClassConectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classconectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassConectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
