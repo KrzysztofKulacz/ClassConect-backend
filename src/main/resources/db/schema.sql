fd5836dd3685dc62324b584ccc71ec93f458abd7
Krzysztof <krzysztof.mckulacz@gmail.com>
2022-10-31 13:05:05 +0100

    DOESN'T WORK !!!

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -12,30 +12,55 @@ CREATE TABLE member (
                        is_account_non_locked BOOLEAN,
                        is_credentials_non_expired BOOLEAN,
                        is_enabled BOOLEAN,
-                       authorities VARCHAR(255)
+                       authorities VARCHAR(255),
+                       post_id UUID NOT NULL
 );
-
-ALTER TABLE member
-    ADD CONSTRAINT PK_member PRIMARY KEY (id);
-
-CREATE TABLE squad (
-                        squad_admin UUID NOT NULL,
-                        subject VARCHAR(255),
-                        password VARCHAR(255)
-);
-
-ALTER TABLE squad
-    ADD CONSTRAINT PK_squad PRIMARY KEY (squad_admin);
 
 CREATE TABLE post (
                       id UUID NOT NULL,
                       title VARCHAR(255),
                       text  VARCHAR(255),
                       member UUID NOT NULL,
-                      squad UUID NOT NULL);
+                      squad UUID NOT NULL
+);
+
+CREATE TABLE squad (
+                       squad_admin UUID NOT NULL,
+                       subject VARCHAR(255),
+                       password VARCHAR(255),
+                       post_id UUID NOT NULL
+);
+
+CREATE TABLE member_squads (
+                               members_id UUID NOT NULL ,
+                               squads_squad_admin UUID NOT NULL
+);
+
+-- ######################################################################
+
+ALTER TABLE member
+    ADD CONSTRAINT PK_member PRIMARY KEY (id);
+
+ALTER TABLE squad
+    ADD CONSTRAINT PK_squad PRIMARY KEY (squad_admin);
 
 ALTER TABLE post
     ADD CONSTRAINT PK_post PRIMARY KEY (id);
+
+ALTER TABLE member_squads
+    ADD CONSTRAINT PK_member_squad PRIMARY KEY (members_id, squads_squad_admin);
+
+-- ######################################################################
+
+ALTER TABLE member
+    ADD CONSTRAINT member_post_fk
+        FOREIGN KEY (post_id)
+            REFERENCES post;
+
+ALTER TABLE squad
+    ADD CONSTRAINT squad_post_fk
+        FOREIGN KEY (post_id)
+            REFERENCES post;
 
 ALTER TABLE post
     ADD CONSTRAINT member_post_fk
@@ -46,13 +71,6 @@ ALTER TABLE post
     ADD CONSTRAINT squad_post_fk
         FOREIGN KEY (squad)
             REFERENCES squad;
-
-CREATE TABLE member_squads (
-    members_id UUID NOT NULL ,
-    squads_squad_admin UUID NOT NULL);
-
-ALTER TABLE member_squads
-    ADD CONSTRAINT PK_member_squad PRIMARY KEY (members_id, squads_squad_admin);
 
 ALTER TABLE member_squads
     ADD CONSTRAINT member_squad_fk
