a7b2a98c90e9c59a83747024bac668dc9711bcf3
Bartłomiej Drobczyk <48151968+BartDro@users.noreply.github.com>
2022-11-09 11:38:17 +0100

    relation member - posts

--- src/test/java/com/example/classconectbackend/ClassConnectBackendApplicationTests.java
+++ src/test/java/com/example/classconectbackend/ClassConnectBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClassConectBackendApplicationTests {
+class ClassConnectBackendApplicationTests {
 
     @Test
     void contextLoads() {
