349b9236dd8d525c2452ec3fcfe7eb91c015ea38
KrzysztofKulacz <krzysztof.mckulacz@gmail.com>
2022-11-15 11:53:44 +0100

    Changed "creation_date" column type from varchar to timestamp

--- src/main/resources/db/schema.04.sql
+++ src/main/resources/db/schema.04.sql
@@ -1,8 +1,8 @@
 ALTER TABLE classconect.public.member
-    ADD COLUMN creation_date VARCHAR(255);
+    ADD COLUMN creation_date TIMESTAMP;
 
 ALTER TABLE classconect.public.team
-    ADD COLUMN creation_date VARCHAR(255);
+    ADD COLUMN creation_date TIMESTAMP;
 
 ALTER TABLE classconect.public.post
-    ADD COLUMN creation_date VARCHAR(255);
+    ADD COLUMN creation_date TIMESTAMP;
