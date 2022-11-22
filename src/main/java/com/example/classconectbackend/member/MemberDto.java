123d6a3b846d3432c002f44cf4d3b859076971d2
KrzysztofKulacz <krzysztof.mckulacz@gmail.com>
2022-11-22 23:29:20 +0100

    Security configured

---
+++ src/main/java/com/example/classconectbackend/member/MemberDto.java
@@ -0,0 +1,4 @@
+package com.example.classconectbackend.member;
+
+public class MemberDto {
+}
---
+++ src/main/java/com/example/classconectbackend/utils/mappers/MemberMapper.java
@@ -0,0 +1,13 @@
+package com.example.classconectbackend.utils.mappers;
+
+
+import com.example.classconectbackend.member.Member;
+import com.example.classconectbackend.member.MemberDto;
+
+public class MemberMapper {
+
+    public static MemberDto mapToDto(Member member){
+
+        return null;
+    }
+}
