c16bc05e6ef995dd9ac6c52049be506c747e71ea
KrzysztofKulacz <krzysztof.mckulacz@gmail.com>
2022-11-25 19:05:39 +0100

    refactor DTO and MemberMapper

--- src/main/java/com/example/classconectbackend/member/MemberDto.java
+++ src/main/java/com/example/classconectbackend/member/MemberDto.java
@@ -1,15 +1,22 @@
 package com.example.classconectbackend.member;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class MemberDto {
 
     private String username;
     private String email;
     private Role role;
+    private List<String> authorities;
+    private LocalDateTime creationDate;
 
-    public MemberDto(String username, String email, Role role) {
+    public MemberDto(String username, String email, Role role, List<String> authorities, LocalDateTime creationDate) {
         this.username = username;
         this.email = email;
         this.role = role;
+        this.authorities = authorities;
+        this.creationDate = creationDate;
     }
 
     public String getUsername() {
@@ -35,4 +42,21 @@ public class MemberDto {
     public void setRole(Role role) {
         this.role = role;
     }
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<String> authorities) {
+        this.authorities = authorities;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
 }
+
--- src/main/java/com/example/classconectbackend/utils/mappers/MemberMapper.java
+++ src/main/java/com/example/classconectbackend/utils/mappers/MemberMapper.java
@@ -3,13 +3,20 @@ package com.example.classconectbackend.utils.mappers;
 
 import com.example.classconectbackend.member.Member;
 import com.example.classconectbackend.member.MemberDto;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.util.stream.Collectors;
+
 public class MemberMapper {
 
     public static MemberDto mapToDto(Member member){
 
-        return new MemberDto(member.getNickName(), member.getEmail(), member.getRole());
+        var  authorities = member.getAuthorities().stream()
+                .map(SimpleGrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        return new MemberDto(member.getNickName(), member.getEmail(), member.getRole(),
+                authorities, member.getCreationDate());
     }
 }
