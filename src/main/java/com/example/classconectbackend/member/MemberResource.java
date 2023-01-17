08e1a95eb669d0ce213dc4e3ac6080a5f7f3a46a
Bartłomiej Drobczyk <48151968+BartDro@users.noreply.github.com>
2023-01-17 13:55:58 +0100

    user leaving group mechanism implemented

--- src/main/java/com/example/classconectbackend/member/MemberResource.java
+++ src/main/java/com/example/classconectbackend/member/MemberResource.java
@@ -1,8 +1,12 @@
 package com.example.classconectbackend.member;
 
+import com.example.classconectbackend.team.TeamDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
@@ -16,12 +20,21 @@ public class MemberResource {
         this.memberService = memberService;
     }
 
-    @GetMapping("/isUserWithinGroup")
-    public ResponseEntity<Boolean> isUserWithinGroup(@RequestParam("groupId")UUID groupId,
-                                                     @RequestParam("userId")UUID userId){
+    @GetMapping("/is-user-within-group")
+    public ResponseEntity<Boolean> isUserWithinGroup(@RequestParam("groupId") UUID groupId,
+                                                     @RequestParam("userId") UUID userId) {
 
-        var isUserPresent = memberService.isUserWithinGroup(userId,groupId);
-        return new ResponseEntity<>(isUserPresent,HttpStatus.OK);
+        var isUserPresent = memberService.isUserWithinGroup(userId, groupId);
+        return new ResponseEntity<>(isUserPresent, HttpStatus.OK);
+    }
+
+    @GetMapping("/remove-from-group")
+    public ResponseEntity<TeamDto> removeMemberFromGroup(@RequestParam("groupId") UUID groupId,
+                                                         @RequestParam("userId") UUID userId) {
+
+        var removedTeam = memberService.removeTeamFromUser(groupId, userId);
+
+        return new ResponseEntity<>(removedTeam, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/classconectbackend/member/MemberService.java
+++ src/main/java/com/example/classconectbackend/member/MemberService.java
@@ -1,5 +1,7 @@
 package com.example.classconectbackend.member;
 
+import com.example.classconectbackend.team.TeamDto;
+import com.example.classconectbackend.utils.mappers.TeamMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,5 +17,23 @@ public class MemberService {
 
     public boolean isUserWithinGroup(UUID userId, UUID groupId) {
         return memberRepository.isUserWithinGroup(userId, groupId);
+    }
+
+    public TeamDto removeTeamFromUser(UUID teamId, UUID memberId) {
+
+        var requestedMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalStateException("User not found"));
+
+        var requestedTeam = requestedMember.getTeams()
+                .stream()
+                .filter(team -> team.getTeamId().equals(teamId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Team not found"));
+
+        requestedMember.removeTeam(requestedTeam);
+
+        memberRepository.save(requestedMember);
+
+        return TeamMapper.mapToDto(requestedTeam);
     }
 }
